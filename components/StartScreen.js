94dde9f3806aa0580772aed5743317bb3797cb1a
atharvarakshak <atharvarakshak23@gmail.com>
2024-11-26 00:53:35 +0530

    start screen

--- components/StartScreen.js
+++ components/StartScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 export default function StartScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <View style={styles.logoOuter}>
-          <Text style={styles.logoText}>T</Text>
-        </View>
-      </View>
+      <Text style={styles.heading}>Task Management App</Text>
+      <Image 
+        source={require('../assets/logo.png')} 
+        style={styles.logo} 
+        resizeMode="contain" 
+      />
       <TouchableOpacity 
         style={styles.startButton} 
         onPress={() => navigation.navigate('Task List')}
@@ -22,38 +23,27 @@ export default function StartScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    borderStyle:'solid',
+    borderBlockColor:'#000000',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ecf0f1',
   },
-  logoContainer: {
+  logo: {
     width: 250,
     height: 250,
-    justifyContent: 'center',
-    alignItems: 'center',
     marginBottom: 50,
   },
-  logoOuter: {
-    width: 200,
-    height: 200,
-    backgroundColor: '#27ae60',
-    borderRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 8,
-  },
-  logoText: {
-    fontSize: 120,
-    color: 'white',
-    fontWeight: 'bold',
-  },
   startButton: {
-    backgroundColor: '#34495e',
+    backgroundColor: '#27ae60',
     paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 10,
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    paddingVertical: 25,
     paddingHorizontal: 50,
     borderRadius: 10,
   },
